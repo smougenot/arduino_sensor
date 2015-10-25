ddd341ff9fc9c09c545ce0d71d86991c61e41387
Sylvain Mougenot <sylvain.mougenot@gmail.com>
2015-10-25 22:02:41 +0100

    traces

--- digit/tp7_segment/tp7_segment.ino
+++ digit/tp7_segment/tp7_segment.ino
@@ -1,6 +1,6 @@
-#define LATCH 10
-#define CLK   11
-#define DATA  12
+#define LATCH 12
+#define CLK   14
+#define DATA  16
 
 //This is the hex value of each number stored in an array by index num
 byte digitOne[10]= {0xDE, 0x06, 0xEC, 0xAE, 0x36, 0xBA, 0xFA, 0x0E, 0xFE, 0xBE};
@@ -8,7 +8,9 @@ byte digitOne[10]= {0xDE, 0x06, 0xEC, 0xAE, 0x36, 0xBA, 0xFA, 0x0E, 0xFE, 0xBE};
 
 
 void setup(){
- 
+   // You can open the Arduino IDE Serial Monitor window to see what the code is doing
+  Serial.begin(115200);  // Serial connection from ESP-01 via 3.3v console cable
+
   pinMode(LATCH, OUTPUT);
   pinMode(CLK,   OUTPUT);
   pinMode(DATA,  OUTPUT);
@@ -21,10 +23,14 @@ void loop(){
 
 void doDigit(){
   for(int i=0; i<10; i++){
+    Serial.print("digit : ");
+    Serial.println(i);
+
+    
       digitalWrite(LATCH, LOW);
       digitalWrite(CLK, LOW);
       shiftOut(DATA, CLK, MSBFIRST, digitOne[i]); // digitOne
       digitalWrite(LATCH, HIGH);
-      delay(700);
+      delay(500);
   }
 }
