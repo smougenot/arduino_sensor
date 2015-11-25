4427a241caf55dc386e18a3a7c7ce2eaf567430a
Sylvain Mougenot <sylvain.mougenot@gmail.com>
2015-11-26 00:17:29 +0100

    Go to bmp180 instead of clumsy DHT
    Component is cheaper (1$) and much more precise and reliable
    It use I2C for reading
     - temperature
     - pressure (atmospheric)
     - altitude

--- thermometre_wifi/dht_mqtt/dht_mqtt.ino
+++ thermometre_wifi/dht_mqtt/dht_mqtt.ino
@@ -1,33 +1,61 @@
-//
+// BMP180
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+// Display
+#include <TM1637Display.h>
 
-#include <dht.h>
+#define PRJ_VERSION 1
 
-dht DHT;
+// display pins
+#define CLK   14
+#define DATA  16
 
-#define DHT22_PIN 5
+// Display
+TM1637Display display(CLK, DATA);
 
-struct
-{
-    uint32_t total;
-    uint32_t ok;
-    uint32_t crc_error;
-    uint32_t time_out;
-    uint32_t connect;
-    uint32_t ack_l;
-    uint32_t ack_h;
-    uint32_t unknown;
-} stat = { 0,0,0,0,0,0,0,0};
+//Sensor
 
-float humidity = -1;
+// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
+// Connect GND to Ground
+// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5 esp8266 -> #5
+// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4  esp8266 -> #4
+// EOC is not used, it signifies an end of conversion
+// XCLR is a reset pin, also not used here
+
+Adafruit_BMP085 bmp;
+
+// Datas
 float temperature = -1;
+float altitude = -1;
+int pressure = -1;
+short loopCnt = 0;
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("dht22 to mqtt");
-    Serial.print("LIBRARY VERSION: ");
-    Serial.println(DHT_LIB_VERSION);
+    Serial.println("sensor to mqtt");
+    Serial.print("VERSION: ");
+    Serial.println(PRJ_VERSION);
     Serial.println();
+
+    //
+    // Display
+    //
+    display.setBrightness(0x0f);
+    // All segments on
+    displayAllOn();
+
+      if (!bmp.begin()) {
+        Serial.println("Could not find a valid BMP180 sensor, check wiring!");
+        // loop will force reboot
+        while (1) {}
+      }
+
+    // première mesure
+    Serial.println("Lancement de la première mesure");  
+    displayAllOff();
+
+    
     Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");
 }
 
@@ -35,69 +63,80 @@ void loop()
 {
     readSensor();
     trace();
-    delay(2000);
+    displayTemperature();
+    delay(300);
 }
 
 void readSensor(){
-
+    Serial.println("Read bpm180");
     // READ DATA
-    Serial.print(" Read DHT22, \t");
-
-    uint32_t start = micros();
-    int chk = DHT.read22(DHT22_PIN);
-    uint32_t stop = micros();
-
-    stat.total++;
-    switch (chk) {
-    case DHTLIB_OK:
-        stat.ok++;
-        Serial.print("OK,\t");
-        humidity = DHT.humidity;
-        temperature = DHT.temperature;
-        break;
-    case DHTLIB_ERROR_CHECKSUM:
-        stat.crc_error++;
-        Serial.print("Checksum error,\t");
-        break;
-    case DHTLIB_ERROR_TIMEOUT:
-        stat.time_out++;
-        Serial.print("Time out error,\t");
-        break;
-    default:
-        stat.unknown++;
-        Serial.print("Unknown error,\t");
-        break;
+    float myTemperature = bmp.readTemperature();
+    if(myTemperature<150){
+      // assumed correct read
+      temperature = myTemperature;
+      pressure = bmp.readPressure();
+      altitude = bmp.readAltitude();
     }
+}
 
+void trace(){
     // DISPLAY DATA
-    Serial.print(DHT.humidity, 1);
-    Serial.print(",\t");
-    Serial.print(DHT.temperature, 1);
-    Serial.print(",\t");
-    Serial.print(stop - start);
-    Serial.println();
+    if(loopCnt++ %20 == 0){
+      printHeader();
+    }
+    printData(temperature, pressure, altitude);  
 }
 
-void trace() {
-    if (stat.total % 10 == 0)
-    {
-        Serial.println("\nTOT\tOK\tCRC\tTO\tUNK");
-        Serial.print(stat.total);
-        Serial.print("\t");
-        Serial.print(stat.ok);
-        Serial.print("\t");
-        Serial.print(stat.crc_error);
-        Serial.print("\t");
-        Serial.print(stat.time_out);
-        Serial.print("\t");
-        Serial.print(stat.connect);
-        Serial.print("\t");
-        Serial.print(stat.ack_l);
-        Serial.print("\t");
-        Serial.print(stat.ack_h);
-        Serial.print("\t");
-        Serial.print(stat.unknown);
-        Serial.println("\n");
-    }
+void printData(float aTemperature, int aPressure, float aAltitude){
+  Serial.print(aTemperature);
+  Serial.print("\t");
+  Serial.print(aPressure);
+  Serial.print("\t");
+  Serial.print(aAltitude);
+  Serial.println("");
 }
+
+void printHeader(){
+  Serial.println("Temperature °C\tPressure\tAltitude");  
+}
+
+
+/**
+ * All segments on the display to on
+ */
+void displayAllOn(){
+  // All segments on
+  displayAll(0xff);
+}
+
+/**
+ * All segments on the display to off
+ */
+void displayAllOff(){
+  // All segments off
+  displayAll(0);
+}
+
+/**
+ * Display same pattern on all segments
+ */
+void displayAll(uint8_t aValue){
+  // All segments on the same value
+  Serial.print("all to ");
+  Serial.println(aValue);
+  uint8_t data[] = { aValue, aValue, aValue, aValue };
+  display.setSegments(data);
+}
+
+/**
+ * Display on the embedded hardware
+ */
+void displayTemperature(){
+    Serial.print("Display temperature : ");  
+    Serial.print(temperature, 1);
+    Serial.println("°C");
+  
+    display.showNumberDec(temperature * 10, false, 4, 0);
+}
+
 
