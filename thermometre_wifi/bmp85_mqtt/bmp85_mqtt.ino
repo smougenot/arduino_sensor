3e6fabec493b86d2ec691cb10444242205bb439e
Sylvain Mougenot <sylvain.mougenot@gmail.com>
2015-12-12 20:45:45 +0100

    mqtt ... killing (reboot) the nodemcu ?!?

--- thermometre_wifi/bmp85_mqtt/bmp85_mqtt.ino
+++ thermometre_wifi/bmp85_mqtt/bmp85_mqtt.ino
@@ -3,8 +3,11 @@
 #include <Adafruit_BMP085.h>
 // Display
 #include <TM1637Display.h>
+// send messages
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-#define PRJ_VERSION 1
+#define PRJ_VERSION 2
 
 // wait time (ms)
 #define LOOP_WAIT 1000
@@ -31,11 +34,26 @@ TM1637Display display(CLK, DATA);
 
 Adafruit_BMP085 bmp;
 
+// MQTT
+#define CLIENT_ID     "2"
+const char* mqtt_server = "192.168.1.12";
+const char* topicCmd    = "/esp/2/cmd";
+const char* topicStatus = "/esp/2/status";
+const char* clientId    = "ESP8266Client2";
+// network.
+const char* ssid = "MaisonSMT";
+const char* password = "m3f13t01";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 // Datas
 float temperature = -1;
 float altitude = -1;
 int pressure = -1;
 short loopCnt = 0;
+// timing
+long lastCheck = 0;
 
 void setup()
 {
@@ -44,10 +62,12 @@ void setup()
     Serial.print("VERSION: ");
     Serial.println(PRJ_VERSION);
     Serial.println();
-    
-    //Wire.pins(int sda, int scl)
-    Wire.pins(IC_DATA, IC_CLK);
 
+    // network
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
+    
     //
     // Display
     //
@@ -55,11 +75,16 @@ void setup()
     // All segments on
     displayAllOn();
 
-      if (!bmp.begin()) {
-        Serial.println("Could not find a valid BMP180 sensor, check wiring!");
-        // loop will force reboot
-        while (1) {}
-      }
+    // Sensor
+    Serial.println("Setting sensor");
+    Wire.pins(IC_DATA, IC_CLK);
+
+    if (!bmp.begin()) {
+      Serial.println("Could not find a valid BMP180 sensor, check wiring!");
+      // loop will force reboot
+      while (1) {}
+    }
+    Serial.println("Sensor ready");
 
     // première mesure
     Serial.println("Lancement de la première mesure");  
@@ -69,12 +94,107 @@ void setup()
     Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");
 }
 
+
 void loop()
 {
+  
+  // check incomming/connection
+  checkClient();
+
+  long now = millis();
+  if (now - lastCheck > LOOP_WAIT) {
+    lastCheck = now;
+    work();
+  }
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(". ");
+    delay(200);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void checkClient() {
+  Serial.println("checkClient");
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+void reconnect() {
+  Serial.println("reconnect");
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientId)) {
+      Serial.print("connected with id : ");
+      Serial.println(clientId);
+      // Once connected, publish an announcement...
+      client.publish(topicStatus, "hello world");
+      Serial.print("subscribing to : ");
+      Serial.print(topicCmd);
+      if(client.subscribe(topicCmd)){
+        Serial.println(" succeeded");
+      }else{
+        Serial.println(" failed");
+      }
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void work(){
+    Serial.println("work");
     readSensor();
     trace();
     displayTemperature();
-    delay(LOOP_WAIT);
+    sendDatas();
+}
+
+void sendDatas(){
+  
 }
 
 void readSensor(){
